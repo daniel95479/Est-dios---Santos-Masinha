6f7a8e6d05a1f3fb16d6f80ade74d5f4295cddc6
daniel95479 <137818944+daniel95479@users.noreply.github.com>
2023-06-27 23:05:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,55 +5,55 @@ const app = express();
 const estadios = [
   {
     Estadio: "Arena Condá",
-    JogadoresNoEstadio: "0"
+    JogadoresNoEstadio: 0
   },
     {
     Estadio: "Arena Indepedência",
-    JogadoresNoEstadio: "0"
+    JogadoresNoEstadio: 0
   },
     {
     Estadio: "Arena Pernambuco",
-    JogadoresNoEstadio: "0"
+    JogadoresNoEstadio: 0
   },
     {
     Estadio: "Arena da Baixada",
-    JogadoresNoEstadio: "0"
+    JogadoresNoEstadio: 0
   },
     {
     Estadio: "Arena do Grêmio",
-    JogadoresNoEstadio: "0"
+    JogadoresNoEstadio: 0
   },
     {
     Estadio: "Estádio Beira Rio",
-    JogadoresNoEstadio: "0"
+    JogadoresNoEstadio: 0
   },
     {
     Estadio: "Ilha do retiro",
-    JogadoresNoEstadio: "0"
+    JogadoresNoEstadio: 0
   },
     {
     Estadio: "Martins Pereira",
-    JogadoresNoEstadio: "0"
+    JogadoresNoEstadio: 0
   },
     {
     Estadio: "Nabi Abi Chedid",
-    JogadoresNoEstadio: "0"
+    JogadoresNoEstadio: 0
   },
     {
     Estadio: "Neoquímica Arena",
-    JogadoresNoEstadio: "0"
+    JogadoresNoEstadio: 0
   },
     {
     Estadio: "Pacaembú",
-    JogadoresNoEstadio: "0"
+    JogadoresNoEstadio:"0
   },
     {
     Estadio: "São Januário",
-    JogadoresNoEstadio: "0"
+    JogadoresNoEstadio: 0
   },
     {
     Estadio: "Vila Belmiro",
-    JogadoresNoEstadio: "0"
+    JogadoresNoEstadio: 0
   },
 ];
 
